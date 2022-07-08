3a47c1e2b93b40e8f4ab3d844d7ffe9f7297bb12
Minh Thuận <48424223+minhthuancute@users.noreply.github.com>
2022-07-08 21:14:55 +0700

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".filters").on("click", "li", function () {
+    // remove all
+    $(".filters li").removeClass("active");
+
+    $(this).addClass("active");
+  });
+
+  component;
+  data: +html + css + js;
+});
